870304dc7659c3f37fcf55d4a04ad606dde55993
knguyenuit <knguyen.uit@gmail.com>
2017-05-30 09:12:13 +0700

    add staff\

---
+++ src/restaurant/panel/staff/JPanelStaffRowItem.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant.panel.staff;
+import restaurant.panel.ppackkage.*;
+import assets.images.ImageResources;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author WINDNCC
+ */
+public class JPanelStaffRowItem extends javax.swing.JPanel {
+    //protected String materialName;
+    protected int maNV;
+    protected String tenNV;
+    protected String soDienThoai;
+    protected String diaChi;
+    protected static Color oddBackground = new Color(224, 224, 235);
+    public JPanelStaffRowItem() {
+        initComponents();
+    }
+    public JPanelStaffRowItem(int staffID, String staffName, String phonenumber, String address, boolean odd) {
+        initComponents();
+        this.maNV = staffID;
+        this.tenNV = staffName;
+        this.soDienThoai = phonenumber;
+        this.diaChi = address;
+        
+        if(odd){
+            setBackground(oddBackground);
+        }
+        customInit();
+    }
+    public final void customInit(){
+        jLabelStaffId.setText(this.maNV + "");
+        jLabelStaffName.setText("<html><p>" + this.tenNV + "</p></html>");
+        jLabelStaffPhoneNumber.setText(this.soDienThoai + " ");
+        jLabelStaffAddress.setText(this.diaChi + " ");
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabelStaffId = new javax.swing.JLabel();
+        jLabelStaffName = new javax.swing.JLabel();
+        jLabelStaffPhoneNumber = new javax.swing.JLabel();
+        jLabelEdit = new javax.swing.JLabel();
+        jLabelDelete = new javax.swing.JLabel();
+        jLabelStaffAddress = new javax.swing.JLabel();
+
+        setMaximumSize(new java.awt.Dimension(760, 57));
+        setMinimumSize(new java.awt.Dimension(760, 57));
+        setPreferredSize(new java.awt.Dimension(760, 57));
+
+        jLabelStaffId.setText("001");
+
+        jLabelStaffName.setText("Thịt xông khói");
+
+        jLabelStaffPhoneNumber.setText("120000000");
+
+        jLabelEdit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/images/icons/icon_edit_green_x24.png"))); // NOI18N
+
+        jLabelDelete.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/images/icons/icon_dell_gray_x24.png"))); // NOI18N
+
+        jLabelStaffAddress.setText("limit");
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabelStaffId, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(26, 26, 26)
+                .addComponent(jLabelStaffName, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(60, 60, 60)
+                .addComponent(jLabelStaffPhoneNumber)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 117, Short.MAX_VALUE)
+                .addComponent(jLabelStaffAddress, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabelEdit)
+                .addGap(18, 18, 18)
+                .addComponent(jLabelDelete)
+                .addGap(46, 46, 46))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(16, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabelDelete)
+                    .addComponent(jLabelEdit))
+                .addGap(17, 17, 17))
+            .addComponent(jLabelStaffAddress, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabelStaffPhoneNumber, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabelStaffName, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jLabelStaffId, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    
+
+    
+
+    public JLabel getjLabelDelete() {
+        return jLabelDelete;
+    }
+
+    public JLabel getjLabelEdit() {
+        return jLabelEdit;
+    }
+
+    
+    public int getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(int maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public String getSoDienThoai() {
+        return soDienThoai;
+    }
+
+    public void setSoDienThoai(String soDienThoai) {
+        this.soDienThoai = soDienThoai;
+    }
+
+    public static Color getOddBackground() {
+        return oddBackground;
+    }
+
+    public static void setOddBackground(Color oddBackground) {
+        JPanelStaffRowItem.oddBackground = oddBackground;
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabelDelete;
+    private javax.swing.JLabel jLabelEdit;
+    private javax.swing.JLabel jLabelStaffAddress;
+    private javax.swing.JLabel jLabelStaffId;
+    private javax.swing.JLabel jLabelStaffName;
+    private javax.swing.JLabel jLabelStaffPhoneNumber;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/view/VStaff.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+import core.CString;
+import database.Database;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author khanhnguyen
+ */
+public class VStaff extends View {
+    public VStaff() {
+    }
+    
+    public VStaff(ViewData viewData) {
+        data = viewData;
+        filterData = (ViewData) data.clone();
+    }
+    
+    public static VStaff getAllStaff(){
+        String sqlQuery = "SELECT nv.MaNV, nv.HoTenNV, nv.SDTNV, nv.DiaChiNV FROM nhan_vien as nv;";
+//        System.out.println(sqlQuery);
+        try{
+            return new VStaff(Database.viewSelect(sqlQuery));
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
+        return new VStaff();
+    }
+    public void filter(String name){
+        super.filter(new FilterView(){
+            @Override
+            public boolean filter(ViewItem o) {
+                String viName = (String) o.get("HoTenNV");
+                viName = CString.removeAccent(viName).toLowerCase();
+                return viName.contains(CString.removeAccent(name).toLowerCase());
+            }
+            
+        });
+    }
+}
